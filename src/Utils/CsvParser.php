f642859db731698731131bd94868f9720c5b9f55
wiktor <contact@wsadowski.pl>
2024-06-29 15:11:27 +0200

    possible fix

--- src/Utils/CsvParser.php
+++ src/Utils/CsvParser.php
@@ -9,22 +9,22 @@ use InvalidArgumentException;
 
 class CsvParser
 {
-   public static function parseCsv(string $filePath): array
-   {
-       if (!file_exists($filePath)) {
-           throw new InvalidArgumentException("Plik $filePath nie istnieje.");
-       }
+    public static function parseCsv(string $filePath): array
+    {
+        if (!file_exists($filePath)) {
+            throw new InvalidArgumentException("Plik $filePath nie istnieje.");
+        }
 
-       $breakpoints = [];
+        $breakpoints = [];
 
-       $file = fopen($filePath, 'r');
-       if ($file) {
-           while (($row = fgetcsv($file)) !== false) {
-               $breakpoints[$row[0]] = (int) $row[1];
-           }
-           fclose($file);
-       }
+        $file = fopen($filePath, 'r');
+        if ($file) {
+            while (($row = fgetcsv($file)) !== false) {
+                $breakpoints[$row[0]] = (int) $row[1];
+            }
+            fclose($file);
+        }
 
-       return $breakpoints;
-   }
+        return $breakpoints;
+    }
 }
